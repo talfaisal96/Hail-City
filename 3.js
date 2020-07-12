f145df554e304568bfed3bc029d07853ea8c1d03
tlfaisal <67852154+tlfaisal@users.noreply.github.com>
2020-07-12 21:10:56 +0300

    Add files via upload

---
+++ 3.js
@@ -0,0 +1,10 @@
+function Hail() {
+  alert("أهلا و سهلاً بكم")}
+
+  function getConfirmation() {
+  var retVal = confirm("هل اعجبتك الجولة ?");
+  if( retVal == true ) {
+  document.write ("شكراً جزيلاً");
+  return true; }
+  else { document.write ("شكراً جزيلاً");
+  return false; } }
